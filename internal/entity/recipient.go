5d0c243b70f75bcd50068f04b788588f00bd333e
TiagoDiass <tiago.costadiasss@gmail.com>
2024-03-21 20:20:06 -0300

    feat: add recipient entity

---
+++ internal/entity/recipient.go
@@ -0,0 +1,35 @@
+package entity
+
+import "github.com/google/uuid"
+
+type Address struct {
+	Street      string
+	HouseNumber string
+	District    string
+	State       string
+	Zipcode     string
+}
+
+type Recipient struct {
+	ID   string
+	Name string
+	Address
+}
+
+func NewAddress(street, houseNumber, district, state, zipcode string) Address {
+	return Address{
+		Street:      street,
+		HouseNumber: houseNumber,
+		District:    district,
+		State:       state,
+		Zipcode:     zipcode,
+	}
+}
+
+func NewRecipient(name string, address Address) Recipient {
+	return Recipient{
+		ID:      uuid.NewString(),
+		Name:    name,
+		Address: address,
+	}
+}
---
+++ internal/entity/recipient_test.go
@@ -0,0 +1,27 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewAddress(t *testing.T) {
+	address := NewAddress("Main St", "123", "Downtown", "CA", "12345")
+
+	require.Equal(t, address.Street, "Main St")
+	require.Equal(t, address.HouseNumber, "123")
+	require.Equal(t, address.District, "Downtown")
+	require.Equal(t, address.State, "CA")
+	require.Equal(t, address.Zipcode, "12345")
+}
+
+func TestNewRecipient(t *testing.T) {
+	address := NewAddress("Main St", "123", "Downtown", "CA", "12345")
+	recipient := NewRecipient("John Doe", address)
+
+	require.NotEmpty(t, recipient.ID)
+	require.Equal(t, recipient.Name, "John Doe")
+	require.Equal(t, recipient.Address.Street, "Main St")
+	require.Equal(t, recipient.Address.HouseNumber, "123")
+}
