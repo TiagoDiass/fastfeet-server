4c9b61decd1c02da443234d553450c3b82a2893c
TiagoDiass <tiago.costadiasss@gmail.com>
2024-03-23 00:24:34 -0300

    test: add tests for user entity

---
+++ internal/entity/user_test.go
@@ -0,0 +1,35 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewUser(t *testing.T) {
+	user := NewUser(
+		"fake-document",
+		"fake-password",
+		"fake-name",
+		"email@example.com",
+		"fake-phone",
+		Admin,
+	)
+
+	require.NotEmpty(t, user.ID)
+	require.Equal(t, user.Document, "fake-document")
+	require.Equal(t, user.Password, "fake-password")
+	require.Equal(t, user.Name, "fake-name")
+	require.Equal(t, user.Email, "email@example.com")
+	require.Equal(t, user.Phone, "fake-phone")
+	require.Equal(t, user.Role, Admin)
+	require.Equal(t, user.Role.String(), "admin") // Verify Role string representation
+}
+
+func TestRoleEnum(t *testing.T) {
+	unknownRole := Role(2)
+
+	require.Equal(t, Admin.String(), "admin")
+	require.Equal(t, DeliveryMan.String(), "deliveryman")
+	require.Equal(t, unknownRole.String(), "unknown")
+}
