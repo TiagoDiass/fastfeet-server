2f2f39de447e23b9bc4f3d03395e8b3a92edf18b
TiagoDiass <tiago.costadiasss@gmail.com>
2024-03-24 22:48:53 -0300

    chore: add interface for users repo

---
+++ internal/repository/user_repository.go
@@ -0,0 +1,10 @@
+package repository
+
+import "github.com/TiagoDiass/fastfeet-server/internal/entity"
+
+type UserRepository interface {
+	Create(user *entity.User) error
+	Update(user *entity.User) error
+	FindById(id string) (*entity.User, error)
+	Delete(id string) error
+}
