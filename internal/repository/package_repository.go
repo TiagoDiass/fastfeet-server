35367437d69713956e3c604c10b22ca7ba5b1358
TiagoDiass <tiago.costadiasss@gmail.com>
2024-06-06 23:35:07 -0300

    chore: comment unused method for now

--- internal/repository/package_repository.go
+++ internal/repository/package_repository.go
@@ -4,6 +4,6 @@ import "github.com/TiagoDiass/fastfeet-server/internal/entity"
 
 type PackageRepository interface {
 	Create(pkg *entity.Package) error
-	FindById(pkgId string) (*entity.Package, error)
+	// FindById(pkgId string) (*entity.Package, error)
 	FindAllAvailablePackages() ([]*entity.Package, error)
 }
--- internal/test/in_memory_package_repository.go
+++ internal/test/in_memory_package_repository.go
@@ -1,49 +1,30 @@
 package test
 
 import (
-	"errors"
-
 	"github.com/TiagoDiass/fastfeet-server/internal/entity"
 )
 
-var (
-	ErrPackageDoesNotExist = errors.New("package does not exist")
-)
-
 type InMemoryPackageRepository struct {
-	packages map[string]*entity.Package
+	packages []*entity.Package
 }
 
 func NewInMemoryPackageRepository() *InMemoryPackageRepository {
 	return &InMemoryPackageRepository{
-		packages: make(map[string]*entity.Package),
+		packages: []*entity.Package{},
 	}
-}
-
-func (r *InMemoryPackageRepository) Create(pkg *entity.Package) error {
-	r.packages[pkg.ID] = pkg
-
-	return nil
-}
-
-func (r *InMemoryPackageRepository) FindById(pkgId string) (*entity.Package, error) {
-	user, exists := r.packages[pkgId]
-
-	if !exists {
-		return nil, ErrPackageDoesNotExist
-	}
-
-	return user, nil
 }
 
 func (r *InMemoryPackageRepository) FindAllAvailablePackages() ([]*entity.Package, error) {
-	var availablePackages []*entity.Package
-
+	availablePackages := []*entity.Package{}
 	for _, pkg := range r.packages {
 		if pkg.Status == "WAITING_WITHDRAW" {
 			availablePackages = append(availablePackages, pkg)
 		}
 	}
-
 	return availablePackages, nil
+}
+
+func (r *InMemoryPackageRepository) Create(pkg *entity.Package) error {
+	r.packages = append(r.packages, pkg)
+	return nil
 }
