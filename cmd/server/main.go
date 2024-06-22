28683dc2f566858c46ca4d138dc8a8af18ae44b6
TiagoDiass <tiago.costadiasss@gmail.com>
2024-06-22 00:03:06 -0300

    refactor: organize code that initializes the server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/TiagoDiass/fastfeet-server/internal/entity"
 	"github.com/TiagoDiass/fastfeet-server/internal/infra/web"
+	"github.com/TiagoDiass/fastfeet-server/internal/repository"
 	repositoryimpl "github.com/TiagoDiass/fastfeet-server/internal/repository/repository_impl"
 	packageUsecase "github.com/TiagoDiass/fastfeet-server/internal/usecase/package"
 	recipientUsecase "github.com/TiagoDiass/fastfeet-server/internal/usecase/recipient"
@@ -14,6 +15,28 @@ import (
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
+
+type Repositories struct {
+	RecipientRepository repository.RecipientRepository
+	UserRepository      repository.UserRepository
+	PackageRepository   repository.PackageRepository
+}
+
+type Usecases struct {
+	CreateUserUsecase              *userUsecase.CreateUserUsecase
+	CreateRecipientUsecase         *recipientUsecase.CreateRecipientUsecase
+	CreatePackageUsecase           *packageUsecase.CreatePackageUsecase
+	ListAvailablePackagesUsecase   *packageUsecase.ListAvailablePackagesUsecase
+	ListDeliveredPackagesUsecase   *packageUsecase.ListDeliveredPackagesUsecase
+	WithdrawPackageUsecase         *packageUsecase.WithdrawPackageUsecase
+	ConfirmDeliveredPackageUsecase *packageUsecase.ConfirmDeliveredPackageUsecase
+}
+
+type Handlers struct {
+	RecipientHandler *web.RecipientHandler
+	UserHandler      *web.UserHandler
+	PackageHandler   *web.PackageHandler
+}
 
 func main() {
 	// cfg, err := configs.LoadConfig(".")
@@ -30,42 +53,78 @@ func main() {
 
 	db.AutoMigrate(&entity.Recipient{}, &entity.User{}, &entity.Package{})
 
-	recipientRepository := repositoryimpl.NewGormRecipientRepository(db)
-	userRepository := repositoryimpl.NewGormUserRepository(db)
-	packageRepository := repositoryimpl.NewGormPackageRepository(db)
-
-	createUserUsecase := userUsecase.NewCreateUserUsecase(userRepository)
-	createRecipientUsecase := recipientUsecase.NewCreateRecipientUsecase(recipientRepository)
-	createPackageUsecase := packageUsecase.NewCreatePackageUsecase(
-		packageRepository,
-		userRepository,
-		recipientRepository,
-	)
-	listAvailablePackagesUsecase := packageUsecase.NewListAvailablePackagesUsecase(packageRepository)
-	listDeliveredPackagesUsecase := packageUsecase.NewListDeliveredPackagesUsecase(packageRepository)
-	withdrawPackageUsecase := packageUsecase.NewWithdrawPackageUsecase(packageRepository, userRepository)
-	confirmDeliveredPackageUsecase := packageUsecase.NewConfirmDeliveredPackageUsecase(packageRepository)
-
-	recipientHandler := web.NewRecipientHandler(createRecipientUsecase)
-	userHandler := web.NewUserHandler(createUserUsecase)
-	packageHandler := web.NewPackageHandler(
-		createPackageUsecase,
-		listAvailablePackagesUsecase,
-		listDeliveredPackagesUsecase,
-		withdrawPackageUsecase,
-		confirmDeliveredPackageUsecase,
-	)
+	repositories := createRepositories(db)
+	usecases := createUsecases(repositories)
+	handlers := createHandlers(usecases)
 
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
 
-	router.Post("/recipients", recipientHandler.CreateRecipient)
-	router.Post("/users", userHandler.CreateUser)
-	router.Post("/packages", packageHandler.CreatePackage)
-	router.Get("/packages/available", packageHandler.ListAvailablePackages)
-	router.Get("/packages/delivered", packageHandler.ListDeliveredPackages)
-	router.Patch("/packages/withdraw/{packageId}", packageHandler.WithdrawPackage)
-	router.Patch("/packages/confirm-delivery/{packageId}", packageHandler.ConfirmDeliveredPackage)
+	initializeRoutes(router, handlers)
 
 	http.ListenAndServe(":8000", router)
+}
+
+func createRepositories(db *gorm.DB) *Repositories {
+	recipientRepository := repositoryimpl.NewGormRecipientRepository(db)
+	userRepository := repositoryimpl.NewGormUserRepository(db)
+	packageRepository := repositoryimpl.NewGormPackageRepository(db)
+
+	return &Repositories{
+		RecipientRepository: recipientRepository,
+		UserRepository:      userRepository,
+		PackageRepository:   packageRepository,
+	}
+}
+
+func createUsecases(repositories *Repositories) *Usecases {
+	createUserUsecase := userUsecase.NewCreateUserUsecase(repositories.UserRepository)
+	createRecipientUsecase := recipientUsecase.NewCreateRecipientUsecase(repositories.RecipientRepository)
+	createPackageUsecase := packageUsecase.NewCreatePackageUsecase(
+		repositories.PackageRepository,
+		repositories.UserRepository,
+		repositories.RecipientRepository,
+	)
+	listAvailablePackagesUsecase := packageUsecase.NewListAvailablePackagesUsecase(repositories.PackageRepository)
+	listDeliveredPackagesUsecase := packageUsecase.NewListDeliveredPackagesUsecase(repositories.PackageRepository)
+	withdrawPackageUsecase := packageUsecase.NewWithdrawPackageUsecase(repositories.PackageRepository, repositories.UserRepository)
+	confirmDeliveredPackageUsecase := packageUsecase.NewConfirmDeliveredPackageUsecase(repositories.PackageRepository)
+
+	return &Usecases{
+		CreateUserUsecase:              createUserUsecase,
+		CreateRecipientUsecase:         createRecipientUsecase,
+		CreatePackageUsecase:           createPackageUsecase,
+		ListAvailablePackagesUsecase:   listAvailablePackagesUsecase,
+		ListDeliveredPackagesUsecase:   listDeliveredPackagesUsecase,
+		WithdrawPackageUsecase:         withdrawPackageUsecase,
+		ConfirmDeliveredPackageUsecase: confirmDeliveredPackageUsecase,
+	}
+}
+
+func createHandlers(usecases *Usecases) *Handlers {
+	recipientHandler := web.NewRecipientHandler(usecases.CreateRecipientUsecase)
+	userHandler := web.NewUserHandler(usecases.CreateUserUsecase)
+	packageHandler := web.NewPackageHandler(
+		usecases.CreatePackageUsecase,
+		usecases.ListAvailablePackagesUsecase,
+		usecases.ListDeliveredPackagesUsecase,
+		usecases.WithdrawPackageUsecase,
+		usecases.ConfirmDeliveredPackageUsecase,
+	)
+
+	return &Handlers{
+		RecipientHandler: recipientHandler,
+		UserHandler:      userHandler,
+		PackageHandler:   packageHandler,
+	}
+}
+
+func initializeRoutes(router *chi.Mux, handlers *Handlers) {
+	router.Post("/recipients", handlers.RecipientHandler.CreateRecipient)
+	router.Post("/users", handlers.UserHandler.CreateUser)
+	router.Post("/packages", handlers.PackageHandler.CreatePackage)
+	router.Get("/packages/available", handlers.PackageHandler.ListAvailablePackages)
+	router.Get("/packages/delivered", handlers.PackageHandler.ListDeliveredPackages)
+	router.Patch("/packages/withdraw/{packageId}", handlers.PackageHandler.WithdrawPackage)
+	router.Patch("/packages/confirm-delivery/{packageId}", handlers.PackageHandler.ConfirmDeliveredPackage)
 }
