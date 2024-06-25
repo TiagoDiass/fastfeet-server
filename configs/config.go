bd61f5b3b44f3501111c4e6a10b4af232e807848
TiagoDiass <tiago.costadiasss@gmail.com>
2024-06-25 20:49:56 -0300

    chore: add configs package

---
+++ configs/config.go
@@ -0,0 +1,41 @@
+// TODO: QUANDO FOR COMMITAR ISSO AQUI, TIRAR O CAMINHO DO GITIGNORE
+
+package configs
+
+import (
+	"github.com/go-chi/jwtauth"
+	"github.com/spf13/viper"
+)
+
+var cfg *conf
+
+type conf struct {
+	WebServerPort string `mapstructure:"WEBSERVER_PORT"`
+	JWTSecret     string `mapstructure:"JWT_SECRET"`
+	JWTExpiresIn  int    `mapstructure:"JWT_EXPIRES_IN"`
+	TokenAuth     *jwtauth.JWTAuth
+}
+
+func LoadConfig(path string) (*conf, error) {
+	viper.SetConfigName("app_config")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(path)
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = viper.Unmarshal(&cfg)
+
+	if err != nil {
+		panic(err)
+	}
+
+	cfg.TokenAuth = jwtauth.New("HS256", []byte(cfg.JWTSecret), nil)
+
+	return cfg, err
+}
