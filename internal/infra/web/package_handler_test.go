2d9ea8dac86f4f0f22d9380e2bfbf57eb77828a3
TiagoDiass <tiago.costadiasss@gmail.com>
2025-02-03 22:06:45 -0300

    test: ensure CreatePackage creates package correctly (now validating authorization)

--- internal/infra/web/package_handler_test.go
+++ internal/infra/web/package_handler_test.go
@@ -1,45 +1,251 @@
 package web
 
-// import (
-// 	"net/http"
-// 	"net/http/httptest"
-// 	"testing"
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
-// 	"github.com/TiagoDiass/fastfeet-server/internal/repository/test"
-// 	"github.com/TiagoDiass/fastfeet-server/internal/usecase/package"
-// 	"github.com/stretchr/testify/require"
-// )
+	"github.com/TiagoDiass/fastfeet-server/internal/entity"
+	"github.com/TiagoDiass/fastfeet-server/internal/test"
+	usecase "github.com/TiagoDiass/fastfeet-server/internal/usecase/package"
+	"github.com/go-chi/jwtauth"
+	"github.com/lestrrat-go/jwx/jwt"
 
-// func TestListAvailablePackagesHandler(t *testing.T) {
-// 	// Setup
-// 	packageRepo := test.NewInMemoryPackageRepository()
-// 	listAvailablePackagesUsecase := usecase.NewListAvailablePackagesUsecase(packageRepo)
-// 	packageHandler := NewPackageHandler(nil, listAvailablePackagesUsecase)
+	"github.com/stretchr/testify/require"
+)
 
-// 	// Add necessary data to repositories
-// 	packageRepo.Create(&entity.Package{
-// 		ID:            "pkg1",
-// 		RecipientId:   "rec1",
-// 		DeliverymanId: "del1",
-// 		Name:          "Package 1",
-// 		Status:        "WAITING_WITHDRAW",
-// 	})
+func makePackageHandlerSut() *PackageHandler {
+	userRepository := test.NewInMemoryUserRepository()
+	recipientRepository := test.NewInMemoryRecipientRepository()
+	packageRepository := test.NewInMemoryPackageRepository()
 
-// 	// Create request
-// 	req := httptest.NewRequest("GET", "/packages/available", nil)
+	// Create users and recipient
+	admin, _ := entity.NewUser(
+		"33872158007",
+		"beautiful-password",
+		"Admin",
+		"admin@example.com",
+		"fake-phone",
+		"admin",
+	)
+	admin.ID = "admin-id"
+	userRepository.Create(admin)
+
+	deliveryman, _ := entity.NewUser(
+		"36799751044",
+		"beautiful-password",
+		"Deliveryman",
+		"deliveryman@example.com",
+		"fake-phone",
+		"deliveryman",
+	)
+	deliveryman.ID = "deliveryman-id"
+	userRepository.Create(deliveryman)
+
+	address := entity.NewAddress("Main St", "123", "Downtown", "CA", "12345")
+	recipient := entity.NewRecipient(
+		"Jane Doe",
+		"jane@example.com",
+		address,
+	)
+	recipient.ID = "recipient-id"
+	recipientRepository.Create(recipient)
+
+	createPackageUsecase := usecase.NewCreatePackageUsecase(
+		packageRepository,
+		userRepository,
+		recipientRepository,
+	)
+	listAvailablePackagesUsecase := usecase.NewListAvailablePackagesUsecase(packageRepository)
+	listDeliveredPackagesUsecase := usecase.NewListDeliveredPackagesUsecase(packageRepository)
+	withdrawPackageUsecase := usecase.NewWithdrawPackageUsecase(packageRepository, userRepository)
+	confirmDeliveredPackageUsecase := usecase.NewConfirmDeliveredPackageUsecase(packageRepository)
+
+	packageHandler := NewPackageHandler(
+		createPackageUsecase,
+		listAvailablePackagesUsecase,
+		listDeliveredPackagesUsecase,
+		withdrawPackageUsecase,
+		confirmDeliveredPackageUsecase,
+	)
+
+	return packageHandler
+}
+
+func generateToken(userID string) jwt.Token {
+	tokenAuth := jwtauth.New("HS256", []byte("secret"), nil)
+	claims := map[string]interface{}{
+		"sub": userID,
+		"user": map[string]interface{}{
+			"id": userID,
+		},
+	}
+	_, tokenString, _ := tokenAuth.Encode(claims)
+	token, _ := tokenAuth.Decode(tokenString)
+
+	return token
+}
+
+func TestPackageHandler_CreatePackageSuccessCase(t *testing.T) {
+	input := usecase.CreatePackageInputDTO{
+		RecipientID: "recipient-id",
+		Name:        "Sample Package",
+	}
+	inputJSON, _ := json.Marshal(input)
+
+	packageHandler := makePackageHandlerSut()
+
+	token := generateToken("admin-id")
+
+	req := httptest.NewRequest(
+		"POST",
+		"/packages",
+		bytes.NewReader(inputJSON),
+	)
+
+	ctx := jwtauth.NewContext(req.Context(), token, nil)
+	req = req.WithContext(ctx)
+
+	w := httptest.NewRecorder()
+
+	packageHandler.CreatePackage(w, req)
+
+	require.Equal(t, http.StatusCreated, w.Code)
+
+	var output entity.Package
+	err := json.NewDecoder(w.Body).Decode(&output)
+
+	require.Nil(t, err)
+	require.NotEmpty(t, output.ID)
+	require.Equal(t, input.RecipientID, output.RecipientId)
+	require.Equal(t, input.Name, output.Name)
+}
+
+// func TestPackageHandler_CreatePackageWithInvalidJSON(t *testing.T) {
+// 	invalidJSON := []byte(`{
+// 		"recipient_id": "recipient-id",
+// 		"name": "Sample Package"
+// 	`) // Missing closing brace
+
+// 	packageHandler := makePackageHandlerSut()
+// 	token := generateToken("admin-id")
+
+// 	// Create a context with the token
+// 	ctx := context.WithValue(context.Background(), jwtauth.TokenCtxKey, token)
+
+// 	req := httptest.NewRequest(
+// 		"POST",
+// 		"/packages",
+// 		bytes.NewReader(invalidJSON),
+// 	)
+// 	req = req.WithContext(ctx)
 // 	w := httptest.NewRecorder()
 
-// 	// Call handler
-// 	packageHandler.ListAvailablePackages(w, req)
+// 	packageHandler.CreatePackage(w, req)
 
-// 	// Check response
-// 	require.Equal(t, http.StatusOK, w.Code)
+// 	require.Equal(t, http.StatusBadRequest, w.Code)
 
-// 	var output []usecase.ListAvailablePackagesOutputDTO
-// 	err := json.NewDecoder(w.Body).Decode(&output)
+// 	var errorResponse Error
+// 	err := json.NewDecoder(w.Body).Decode(&errorResponse)
+
 // 	require.Nil(t, err)
-// 	require.Len(t, output, 1)
-// 	require.Equal(t, "pkg1", output[0].ID)
-// 	require.Equal(t, "Package 1", output[0].Name)
-// 	require.Equal(t, "WAITING_WITHDRAW", output[0].Status)
+// 	require.NotEmpty(t, errorResponse.Message)
+// }
+
+// func TestPackageHandler_CreatePackageWhenUserIsNotAdmin(t *testing.T) {
+// 	input := usecase.CreatePackageInputDTO{
+// 		RecipientID: "recipient-id",
+// 		Name:        "Sample Package",
+// 	}
+// 	inputJSON, _ := json.Marshal(input)
+
+// 	packageHandler := makePackageHandlerSut()
+// 	token := generateToken("deliveryman-id")
+
+// 	// Create a context with the token
+// 	ctx := context.WithValue(context.Background(), jwtauth.TokenCtxKey, token)
+
+// 	req := httptest.NewRequest(
+// 		"POST",
+// 		"/packages",
+// 		bytes.NewReader(inputJSON),
+// 	)
+// 	req = req.WithContext(ctx)
+// 	w := httptest.NewRecorder()
+
+// 	packageHandler.CreatePackage(w, req)
+
+// 	require.Equal(t, http.StatusUnauthorized, w.Code)
+
+// 	var errorResponse Error
+// 	err := json.NewDecoder(w.Body).Decode(&errorResponse)
+
+// 	require.Nil(t, err)
+// 	require.Equal(t, errorResponse.Message, usecase.ErrUserIsNotAdmin.Error())
+// }
+
+// func TestPackageHandler_CreatePackageWhenRecipientDoesNotExist(t *testing.T) {
+// 	input := usecase.CreatePackageInputDTO{
+// 		RecipientID: "non-existent-recipient-id",
+// 		Name:        "Sample Package",
+// 	}
+// 	inputJSON, _ := json.Marshal(input)
+
+// 	packageHandler := makePackageHandlerSut()
+// 	token := generateToken("admin-id")
+
+// 	// Create a context with the token
+// 	ctx := context.WithValue(context.Background(), jwtauth.TokenCtxKey, token)
+
+// 	req := httptest.NewRequest(
+// 		"POST",
+// 		"/packages",
+// 		bytes.NewReader(inputJSON),
+// 	)
+// 	req = req.WithContext(ctx)
+// 	w := httptest.NewRecorder()
+
+// 	packageHandler.CreatePackage(w, req)
+
+// 	require.Equal(t, http.StatusBadRequest, w.Code)
+
+// 	var errorResponse Error
+// 	err := json.NewDecoder(w.Body).Decode(&errorResponse)
+
+// 	require.Nil(t, err)
+// 	require.Equal(t, errorResponse.Message, usecase.ErrRecipientNotExists.Error())
+// }
+
+// func TestPackageHandler_CreatePackageWhenRepositoryReturnsAnError(t *testing.T) {
+// 	input := usecase.CreatePackageInputDTO{
+// 		RecipientID: "recipient-id",
+// 		Name:        test.NameThatReturnsErrorOnCreatePackage,
+// 	}
+// 	inputJSON, _ := json.Marshal(input)
+
+// 	packageHandler := makePackageHandlerSut()
+// 	token := generateToken("admin-id")
+
+// 	// Create a context with the token
+// 	ctx := context.WithValue(context.Background(), jwtauth.TokenCtxKey, token)
+
+// 	req := httptest.NewRequest(
+// 		"POST",
+// 		"/packages",
+// 		bytes.NewReader(inputJSON),
+// 	)
+// 	req = req.WithContext(ctx)
+// 	w := httptest.NewRecorder()
+
+// 	packageHandler.CreatePackage(w, req)
+
+// 	require.Equal(t, http.StatusInternalServerError, w.Code)
+
+// 	var errorResponse Error
+// 	err := json.NewDecoder(w.Body).Decode(&errorResponse)
+
+// 	require.Nil(t, err)
+// 	require.Equal(t, errorResponse.Message, test.ErrOnCreatePackage.Error())
 // }
